7f24184fb8cbbae1e2c79a03e52143055997fa77
Kingsley <kingsleyo1111@gmail.com>
2018-08-22 09:01:56 +0100

    Yet another change

--- site/content/_index.md
+++ site/content/_index.md
@@ -11,16 +11,5 @@ blurb:
     or maybe day. This calls for the need to keep up with these changes. And you
     know, keeping up with the changes means learning lots of tools - new tools.
     So, how are we going to do that?
-values:
-  heading: How to Keep up
-=======
-    We can keep up with the changes through lots of means. Which include:
-    Googling, YouTube videos, podcasts, blog posts, news letters and many
-    others. Sometimes, the noise out there might even prevent you from knowing
-    the real stuff. There're too much noise,seriously. This again calls for the
-    need to filter the information we get trough those means mentioned above.
-    And how do we do this? The truth is that I don't know. I would tell you to
-    follow your heart.
-    imageUrl: "/img/MordernWeb.jpg"
 ---
 
--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,67 @@
 // JS Goes here - ES6 supported
+
+// Initialize Firebase
+let config = {
+  apiKey: "AIzaSyD9h3vQlFMD27lwFZaryDeYyOkotOmhUIk",
+  authDomain: "contactform-a488c.firebaseapp.com",
+  databaseURL: "https://contactform-a488c.firebaseio.com",
+  projectId: "contactform-a488c",
+  storageBucket: "contactform-a488c.appspot.com",
+  messagingSenderId: "473349870319"
+};
+firebase.initializeApp(config);
+
+//Refrence messages collection
+let messagesRef = firebase.database().ref("messages");
+
+//Listen for form submit
+document.getElementByid("contactForm").addEventListener(
+	"submit", submitForm
+);
+
+//Submit form
+function submitForm(e){
+	e.preventDefault();
+
+	//Get input value
+	let name = getInputVal(name);
+	let email = getInputVal(email);
+	let message = getInputVal(message);
+
+	saveMessage(name, email, message);
+
+	//Show alert
+	document.querySelector(".alert").style.display = "block";
+
+	//Hide alert after 3 sec
+	setTimeout(function(){
+		document.querySelector(".alert").style.display = "none";
+	}, 3000);
+
+	//Clear form inputs
+	document.getElementByid("contactForm").reset();
+
+}
+
+//Function to get input value
+function getInputVal(id) {
+	return document.getElementByid(id).value;
+}
+
+//Save the message firebase
+function saveMessage(name, email, message){
+	let newMessageRef = messagesRef.push();
+	newMessageRef.set({
+		name: name,
+		email: email,
+		message: message
+	});
+
+}
+
+
+//Connetion to Netlify CMS
+
 if (window.netlifyIdentity) {
   window.netlifyIdentity.on("init", user => {
     if (!user) {
